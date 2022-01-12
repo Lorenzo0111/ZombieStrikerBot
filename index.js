2b96ef0da0f7adbb9cc2e2a5db7adc0628c25301
Lorenzo <69311874+Lorenzo0111@users.noreply.github.com>
2022-01-12 17:48:03 +0100

    Fixed status setting

--- index.js
+++ index.js
@@ -109,7 +109,7 @@ fs.readdirSync('modules')
 client.once('ready', () => {
     client.user.setStatus('online');
 
-    client.user.setActivity(config.astatus, { type: "WATCHING" });
+    client.user.setActivity(config.status, { type: "WATCHING" });
 
 	console.log('\n[»] Discord bot is now online!');
 
